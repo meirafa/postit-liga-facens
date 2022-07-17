a5b00215733532f5acf4f494abe601920626a99f
rafa meira <meirarafaela@hotmail.com>
2022-07-17 09:33:03 -0300

    add alert and toast

--- postit/src/app/pages/login/login.page.ts
+++ postit/src/app/pages/login/login.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import {LoginPayload} from 'src/app/models/payload/login.payload'
+import { AlertController, ToastController } from '@ionic/angular';
+import { LoginPayload } from 'src/app/models/payload/login.payload'
 
 @Component({
   selector: 'app-login',
@@ -8,15 +9,46 @@ import {LoginPayload} from 'src/app/models/payload/login.payload'
 })
 export class LoginPage {
 
-  constructor() { }
+  constructor(
+    private readonly toastController: ToastController,
+    private readonly alertController: AlertController,) { }
 
   public loginPayload: LoginPayload = {
     email: '',
     password: '',
   }
 
-  public login(): void {
+  public isLoading: boolean = false;
+
+  public async login(): Promise<void> {
+    this.isLoading = true;
+    //toast
+    const toast = await this.toastController.create({
+      message: 'Logando...',
+      duration: 2000 //2s
+    });
+    toast.present();
+    //alert
+    const alert = await this.alertController.create({
+      header: 'Hello World',
+      buttons: [{
+        text: 'OK',
+        handler: () => { console.log('ok') }
+      }]
+    });
+    alert.present();
     console.log(this.loginPayload);
+  }
+
+  public canLogin(): boolean {
+    const regexEmail = new RegExp('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$');
+
+    const emailIsValid = regexEmail.test(this.loginPayload.email)
+
+    if (emailIsValid && this.loginPayload.password.length >= 6)
+      return true;
+
+    return false;
   }
 
 }
