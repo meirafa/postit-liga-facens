01a5f244c0c06f103887369b47229a52ef12cd6b
rafa meira <meirarafaela@hotmail.com>
2022-07-17 14:29:03 -0300

    update(navbar) working

--- postit/src/app/app.component.ts
+++ postit/src/app/app.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { NavigationEnd, Router } from '@angular/router';
+import { filter } from 'rxjs/operators';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +9,21 @@ import { Component } from '@angular/core';
   styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
-  constructor() {}
+  constructor(
+    private readonly router: Router,
+  ) {
+    router.events.pipe(filter(event => event instanceof NavigationEnd)).subscribe((route: NavigationEnd) => {
+      
+      if (!this.routesWithoutNavbar.includes(route.url)) {
+        this.canShowNavbar = true;
+      } else {
+        this.canShowNavbar = false;
+      }
+
+      // if(route.url !== '/login') this.canShowNavbar = true;
+    })
+  }
+
+  public routesWithoutNavbar: string[] = ['/login'];
+  public canShowNavbar: boolean = false;
 }
